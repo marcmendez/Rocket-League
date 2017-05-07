a88e23460ece0cadf021a9a79aa3c62d51431518
Jordi Estapé Canal <estape.jordi@gmail.com>
2017-05-07 17:40:58 +0200

    ball collisioion + bouncy

---
+++ Assets/Assets/Scripts/BallController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnCollisionEnter(Collision collision) {
+
+		if (collision.gameObject.tag == "Cars") {
+			
+			int magnitude = 5000;
+			var force = transform.position - collision.transform.position;
+			force.Normalize ();
+			gameObject.GetComponent<Rigidbody> ().AddForce (force * magnitude);
+		}
+	}
+}
